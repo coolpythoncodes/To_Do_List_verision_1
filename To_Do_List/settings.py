49b82bfd82ebcf85f39b8497614ef81a75a08158
coolpythoncodes <chijiokeibe15@gmail.com>
2018-12-08 05:40:25 +1200

    Added To-do app in installed_apps in settings.py

--- To_Do_List/settings.py
+++ To_Do_List/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "To_do_app.apps.ToDoAppConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'To_Do_List.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': ["templates "],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
