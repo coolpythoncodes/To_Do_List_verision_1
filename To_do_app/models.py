0a3fe0284ced85ed7d7c30bbd12c89864767a258
coolpythoncodes <chijiokeibe15@gmail.com>
2018-12-08 05:37:56 +1200

    Added model fields for the To-do-list app

--- To_do_app/models.py
+++ To_do_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+    activity = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return self.activity
