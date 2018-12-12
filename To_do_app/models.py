e8156fd98fe440d376b4be257593d0d8867fa53c
coolpythoncodes <chijiokeibe15@gmail.com>
2018-12-13 09:52:45 +1200

    did minor changes to the webapp

--- To_do_app/models.py
+++ To_do_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class List(models.Model):
     activity = models.CharField(max_length=200,blank="True")
     completed = models.BooleanField(default=False)
-    #time = models.DateTimeField()
+    
 
     def __str__(self):
         return self.activity
